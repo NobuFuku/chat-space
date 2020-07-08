c5fe470c7e22b6844da61a82aa148036e6722f93
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-07-08 17:00:49 +0900

    Ajax1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="message-list-name">
+            ${message.user_name}
+          </div>
+          <div        class="message-list-name__calender">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-text">
+          <p class="message__content">
+            ${message.content}
+          </p>
+          <img class="message__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+    }
+    else {
+      let html =
+      `<div class="message-box">
+      <div class="message-info">
+        <div class="message-list-name">
+          ${message.user_name}
+        </div>
+        <div        class="message-list-name__calender">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message-text">
+        <p class="message__content">
+          ${message.content}
+        </p>
+      </div>
+    </div>`
+    return html;
+    }
+  }
+  $('.form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data)
+      $('.MessageField').append(html);      
+      $('form')[0].reset();
+      $('form__Btn').prop('disabled', false);
+      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  });
+});

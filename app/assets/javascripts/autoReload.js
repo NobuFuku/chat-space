25898f8c9d5edc6d694ce04daf4faf5d54582b59
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-07-09 17:37:44 +0900

    bug1

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -43,6 +43,7 @@ $(function(){
   
   let reloadMessages = function() {
     let last_message_id = $('.message-box:last').data("message-id");
+    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',

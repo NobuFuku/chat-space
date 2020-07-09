f825183a57b426f0c6b73c0352372f7356904767
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-07-09 15:34:44 +0900

    Organize codes inside main chat haml.

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -42,7 +42,7 @@ $(function(){
   }
   
   let reloadMessages = function() {
-    let last_message_id = $('.MessageBox:last').data("message-id");
+    let last_message_id = $('.message-box:last').data("message-id");
     $.ajax({
       url: "api/messages",
       type: 'get',
